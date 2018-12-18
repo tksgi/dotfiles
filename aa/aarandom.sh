f6135cf47aae62b004f1ac7dec3ad27d73d311bc
oTetsuKasugai <tetsu.kasugai@framgia.com>
2018-12-18 16:45:36 +0900

    スクリプト実行時に自ファイルが呼ばれるのを解決

--- aa/aarandom.sh
+++ aa/aarandom.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-files=(`\find ~/.aa -type f`)
+files=(`\find ~/.aa -type f -and -not -name \*.sh`)
 clear
 cat ${files[$((RANDOM%${#files[*]}))]}
 # more ${files[$((RANDOM%${#files[*]}))]}
