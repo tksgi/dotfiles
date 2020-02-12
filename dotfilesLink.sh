f1fd02fb56d6406c1017dff040ec0cc27fa46708
tksgi <23550705+tksgi@users.noreply.github.com>
2020-02-12 17:54:31 +0900

    vimのautoloadを追加

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -6,3 +6,4 @@ ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
+ln -sf ~/dotfiles/vim/after ~/.vim/after
