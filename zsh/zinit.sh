0df74af2cf6016f6c37599946d28293c5414dc2f
tksgi <23550705+tksgi@users.noreply.github.com>
2020-03-07 23:35:58 +0900

    git ssh config

--- zsh/zinit.sh
+++ zsh/zinit.sh
@@ -1,7 +1,7 @@
 if [ ! -e $HOME/.zinit/bin ]
 then
   mkdir ~/.zinit
-  git clone https://github.com/zdharma/zinit.git ~/.zinit/bin
+  git clone git@github.com:/zdharma/zinit.git ~/.zinit/bin
 fi
 ### zinit
 source ~/.zinit/bin/zinit.zsh
@@ -25,7 +25,7 @@ zinit ice wait atinit"zpcompinit; zpcdreplay"
 zinit light zdharma/fast-syntax-highlighting
 
 zinit ice as"completion"
-zinit snippet https://github.com/docker/cli/blob/master/contrib/completion/zsh/_docker
+zinit snippet git@github.com:/docker/cli/blob/master/contrib/completion/zsh/_docker
 
 zinit ice as"completion"
 zinit snippet https://raw.githubusercontent.com/docker/compose/1.24.1/contrib/completion/zsh/_docker-compose
