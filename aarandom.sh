ff38d34be34d802a8cdb2bc9c0ecd001315061a5
tksgi <23550705+tksgi@users.noreply.github.com>
2020-11-25 23:32:09 +0900

    syntax error 修正

--- aarandom.sh
+++ aarandom.sh
@@ -1,8 +1,7 @@
-#!/bin/zsh
-files=(`\find ~/dotfiles/aa -type f`)
-index=`expr $((RANDOM%${#files[*]})) + 1`
-echo $index
+#!/bin/sh
+files+=(`\find ~/dotfiles/aa -type f`)
+file_count=`\find ~/dotfiles/aa -type f | wc -l`
+index=`expr $((RANDOM%$file_count)) + 1`
 file=${files[$index]}
-echo $file
 cat $file
 # more ${files[$((RANDOM%${#files[*]}))]}
