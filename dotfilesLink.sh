04528ac0c4d5ee6ec62efd33d41788b0b9a70680
tksgi <23550705+tksgi@users.noreply.github.com>
2020-12-18 11:36:53 +0900

    fix install script

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -8,5 +8,6 @@ ln -sf ~/dotfiles/.gitignore_global ~/.gitignore_global
 ln -sf ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -sf ~/dotfiles/vim/.vimrc ~/.vimrc
 ln -sf ~/dotfiles/nvim ~/.config/nvim
+mkdir -p ~/.vim/after
 ln -sf ~/dotfiles/vim/after ~/.vim/after
 ln -sf ~/dotfiles/alacritty ~/.config/alacritty
