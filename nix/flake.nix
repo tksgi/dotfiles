da02ea17c1be223d8a26205c67285f777b8d39be
tksgi <23550705+tksgi@users.noreply.github.com>
2023-01-14 05:46:34 +0900

    add delta, nix formatter, format nix files

--- nix/flake.nix
+++ nix/flake.nix
@@ -2,7 +2,7 @@
   description = "Home Manager configuration";
 
   inputs = {
-# Specify the source of Home Manager and Nixpkgs.
+    # Specify the source of Home Manager and Nixpkgs.
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -28,47 +28,48 @@
   };
 
   outputs = { nixpkgs, home-manager, flake-utils, ... }@inputs:
-  let
-    system = "x86_64-linux";
-    overlay = (final: prev: {
-      vimPlugins = prev.vimPlugins // {
-        vimdoc-ja = buildVimPluginFrom2Nix {
-          pname = "vimdoc-ja";
-          version = "2023-01-14";
-          src = inputs.vimdoc-ja;
+    let
+      system = "x86_64-linux";
+      overlay = (final: prev: {
+        vimPlugins = prev.vimPlugins // {
+          vimdoc-ja = buildVimPluginFrom2Nix {
+            pname = "vimdoc-ja";
+            version = "2023-01-14";
+            src = inputs.vimdoc-ja;
+          };
+          denops = buildVimPluginFrom2Nix {
+            pname = "denops";
+            version = "2023-01-14";
+            src = inputs.denops;
+          };
+          skkeleton = buildVimPluginFrom2Nix {
+            pname = "skkeleton";
+            version = "2023-01-14";
+            src = inputs.skkeleton;
+          };
+          cmp-skkeleton = buildVimPluginFrom2Nix {
+            pname = "cmp-skkeleton";
+            version = "2023-01-14";
+            src = inputs.cmp-skkeleton;
+          };
         };
-        denops = buildVimPluginFrom2Nix {
-          pname = "denops";
-          version = "2023-01-14";
-          src = inputs.denops;
-        };
-        skkeleton = buildVimPluginFrom2Nix {
-          pname = "skkeleton";
-          version = "2023-01-14";
-          src = inputs.skkeleton;
-        };
-        cmp-skkeleton = buildVimPluginFrom2Nix {
-          pname = "cmp-skkeleton";
-          version = "2023-01-14";
-          src = inputs.cmp-skkeleton;
-        };
+      });
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [ overlay ];
       };
-    });
-    pkgs = import nixpkgs {
-      inherit system;
-      overlays = [ overlay ];
-    };
-    inherit (pkgs.vimUtils) buildVimPluginFrom2Nix;
-  in {
-    homeConfigurations.tetsu = home-manager.lib.homeManagerConfiguration {
-      inherit pkgs;
+      inherit (pkgs.vimUtils) buildVimPluginFrom2Nix;
+    in
+    {
+      homeConfigurations.tetsu = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
 
-# Specify your home configuration modules here, for example,
-# the path to your home.nix.
-      modules = [ ./home.nix ];
+        # Specify your home configuration modules here, for example,
+        # the path to your home.nix.
+        modules = [ ./home.nix ];
 
-# Optionally use extraSpecialArgs
-# to pass through arguments to home.nix
+        # Optionally use extraSpecialArgs
+        # to pass through arguments to home.nix
+      };
     };
-  };
 }
