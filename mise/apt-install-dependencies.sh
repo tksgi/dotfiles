00fed982f555b65268f64f2caf608423aeb0f5b9
tksgi <23550705+tksgi@users.noreply.github.com>
2024-05-18 23:04:00 +0900

    asdfやrtxの設定ファイルをmiseに変更

---
+++ mise/apt-install-dependencies.sh
@@ -0,0 +1,5 @@
+sudo apt install \
+	build-essential libssl-dev zlib1g-dev \
+	libbz2-dev libreadline-dev libsqlite3-dev curl git \
+	libncursesw5-dev xz-utils tk-dev libxml2-dev libxmlsec1-dev libffi-dev liblzma-dev \
+	zip libyaml-dev libreadline6-dev libgmp-dev libncurses5-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
