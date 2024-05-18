048c41487616bed21e6170af9c4026ebed9fefb4
tksgi <23550705+tksgi@users.noreply.github.com>
2024-05-18 23:38:23 +0900

    dotfileのフォルダをXDG_CONFIG_HOMEに指定する様に設定及びこのリポジトリをwhitelist形式でgitignoreする様に設定

--- nvim/lua/plugins/project-config.lua
+++ nvim/lua/plugins/project-config.lua
@@ -1,6 +1,10 @@
 ---@type LazySpec
 local spec = {
   'https://github.com/windwp/nvim-projectconfig',
-  config = true,
+  config = function()
+    require('nvim-projectconfig').setup({
+      project_dir = "~/.config/projects-config/",
+    })
+  end,
 }
 return spec
