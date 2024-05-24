2346aeca824608ed0f38c3271a8556c4f35ea600
tksgi <23550705+tksgi@users.noreply.github.com>
2024-05-24 10:32:42 +0900

    改行してformatを便利化するpluginを追加

---
+++ nvim/lua/plugins/treesj.lua
@@ -0,0 +1,8 @@
+---@type LazySpec
+local spec = {
+  'Wansmer/treesj',
+  keys = { '<space>m', '<space>j', '<space>s' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = true,
+}
+return spec
